106f81746837ee0e18ceccd8ec161746308802d0
oliver <oliver@gmail.com>
2023-09-26 16:14:12 +0800

    update

--- playground/flappy_brid/src/main.rs
+++ playground/flappy_brid/src/main.rs
@@ -18,16 +18,55 @@ impl State {
             mode: GameMode::Menu,
         }
     }
+
+    /// 將遊戲狀態設為 End
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+
+    /// 將遊戲狀態設為 Playing
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        // 清理屏幕
-        ctx.cls();
-
-        // 遊戲屏幕座標系從左上角開始，x 往右遞增，y 往下遞增
-        // print(x, y, "text") 顯示文字
-        ctx.print(1, 1, "Hello Bracket Terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.dead(ctx),
+            GameMode::End => self.play(ctx),
+        }
     }
 }
 
